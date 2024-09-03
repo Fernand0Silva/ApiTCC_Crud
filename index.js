4f133380f8cc6dbdf7ce7cc5e1abded1e70f901b
Luiz <luizfernando3995@gmail.com>
2024-09-03 16:11:16 -0300

    produto criado e artesao atualizado

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/',(req,res) => {
-    return res.json('Hello world');
+    return res.json('Servidor Iniciado :)');
 
 });
 
@@ -13,11 +13,11 @@ let artesao = [];
 //Criar artesao
 
 app.post('/artesao',(req,res) => {
-    const {Id, Nome,Cidade} = req.body;
-    if(!Nome || !Cidade){
-        return res.status(400).send('Nome ou Cidade ausentes')
+    const {Nome,Cidade,Rg,Cpf,Email,Telefone,Endereco,Idade} = req.body;
+    if(!Nome || !Cidade|| !Rg|| !Cpf|| !Email|| !Telefone|| !Endereco|| !Idade){
+        return res.status(400).send('Nome,Cidade,Rg,Cpf,Email,Telefone,Endereço ou Idade ausentes')
     }
-    const novoArtesao = {id: artesao.length + 1, Nome, Cidade};
+    const novoArtesao = {id: artesao.length + 1, Nome, Cidade, Rg, Cpf, Email, Telefone, Endereco, Idade};
     artesao.push(novoArtesao);
     res.status(201).send(novoArtesao);
 });
@@ -40,12 +40,28 @@ app.put('/artesao/:id',(req,res) =>{
         return res.status(404).send('Artesão não encontrado');
     }
 
-    const {Nome,Cidade} = req.body;
+    const {Nome,Cidade,Rg,Cpf,Email,Telefone,Endereco,Idade} = req.body;
     artesaoEncontrado.Nome = Nome || artesaoEncontrado.Nome;
     artesaoEncontrado.Cidade = Cidade || artesaoEncontrado.Cidade;
+    artesaoEncontrado.Rg = Rg || artesaoEncontrado.Rg;
+    artesaoEncontrado.Cpf = Cpf || artesaoEncontrado.Cpf;
+    artesaoEncontrado.Email = Email || artesaoEncontrado.Email;
+    artesaoEncontrado.Telefone = Telefone|| artesaoEncontrado.Telefone;
+    artesaoEncontrado.Endereco = Endereco|| artesaoEncontrado.Endereco;
+    artesaoEncontrado.Idade = Idade|| artesaoEncontrado.Idade;
     
     res.send(artesaoEncontrado);
 });
+/* {
+    "Nome":"José",
+    "Cidade":"SP",
+    "Rg":19809877,
+    "Cpf":1980999,
+    "Email":"joseArtesanato@gmail.com ",
+    "Telefone":11984417588,
+    "Endereco":"Rua cel dilermano Brisola ",
+    "Idade":20
+}*/
 //Deletar Artesão
 app.delete('/artesao/:id',(req,res) =>{
     const artesaoIndex = artesao.findIndex(p => p.id === parseInt(req.params.id));
@@ -54,7 +70,51 @@ app.delete('/artesao/:id',(req,res) =>{
     }
 
      artesao.splice(artesaoIndex, 1);
-     res.status(204).send();
+     res.status(204).send('Produto deletado com sucesso :)');
+});
+//Crud Produto
+
+let produto = [];
+
+app.post('/produto',(req,res) => {
+    const {Nome,Descricao,Preco}= req.body;
+    if(!Nome||!Descricao||!Preco){
+        return res.status(400).send('Nome,Descricao ou Preço ausentes');
+    }
+    const novoProduto ={id: produto.length+1,Nome,Descricao,Preco}
+    produto.push (novoProduto);
+    res.status(201).send(novoProduto);
+});
+app.get('/produto',(req,res) =>{
+    res.json(produto);
+});
+app.get('/produto/:id',(req,res) =>{
+    const prod = produto.find(p => p.id === parseInt(req.params.id));
+    if(!prod){
+        return res.status(404).send('Produto não encontrado -_-');
+    }
+    res.json(prod);
+});
+app.put('/produto/:id',(req,res) =>{
+    const produtoEncontrado = produto.find(p => p.id === parseInt(req.params.id));
+    if(!produtoEncontrado){
+        return res.status(404).send('Produto não encontrado -_-');
+    }
+    const {Nome,Descricao,Preco} = req.body;
+    produtoEncontrado.Nome = Nome || produtoEncontrado.Nome;
+    produtoEncontrado.Descricao = Descricao || produtoEncontrado.Descricao;
+    produtoEncontrado.Preco = Preco || produtoEncontrado.Preco;
+
+    res.send(produtoEncontrado);
+});
+app.delete('/produto/:id',(req,res) =>{
+    const deletarProduto = produto.find(p => p.id === parseInt(req.params.id));
+    if(deletarProduto === -1){
+        return res.status(404).send('Produto não encontrado -_-');
+    }
+    produto.splice(deletarProduto,1);
+    res.status(202).send('Produto deletado com sucesso :)');
+    
 });
 
 app.listen(port, () =>{
